3a6bedfe71b4097ff89a9ab979cf083788d25039
Alvaro Prieto <hyspanic@gmail.com>
2011-12-15 17:06:40 -0300

    colResizable 1.3

--- source/colResizable-1.3.source.js
+++ source/colResizable-1.3.source.js
@@ -6,7 +6,7 @@
     | (_| (_) | | | \ \  __/\__ \ |/ / (_| | |_) | |  __/
      \___\___/|_|_|  \_\___||___/_/___\__,_|_.__/|_|\___|
 	 
-	v 1.2 - a jQuery plugin by Alvaro Prieto Lauroba
+	v 1.3 - a jQuery plugin by Alvaro Prieto Lauroba
 	
 	Licences: MIT & GPL
 	Feel free to use or modify this plugin as far as my full name is kept	
@@ -33,9 +33,11 @@
 	var I = parseInt;
 	var M = Math;
 	var ie =$.browser.msie;
+	var S;
+	try{S = sessionStorage;}catch(e){}	//Firefox crashes when executed as local file system
 	
 	//append required CSS rules  
-	h.append("<style type='text/css'>  .JColResizer{table-layout:fixed;} .JColResizer td, .JColResizer th{overflow:hidden; padding-left:0!important; padding-right:0!important;}  .JCLRgrips{ height:0px; position:relative;} .JCLRgrip{margin-left:-5px; position:absolute; z-index:5; } .JCLRgrip .JColResizer{position:absolute;background-color:red;filter:alpha(opacity=1);opacity:0;width:10px;cursor:e-resize;height:100%;top:0px} .JCLRLastGrip{position:absolute; width:1px; } .JCLRgripDrag{ border-left:1px dotted black;	}</style>");
+	h.append("<style type='text/css'>  .JColResizer{table-layout:fixed;} .JColResizer td, .JColResizer th{overflow:hidden;padding-left:0!important; padding-right:0!important;}  .JCLRgrips{ height:0px; position:relative;} .JCLRgrip{margin-left:-5px; position:absolute; z-index:5; } .JCLRgrip .JColResizer{position:absolute;background-color:red;filter:alpha(opacity=1);opacity:0;width:10px;height:100%;top:0px} .JCLRLastGrip{position:absolute; width:1px; } .JCLRgripDrag{ border-left:1px dotted black;	}</style>");
 
 	
 	/**
@@ -43,18 +45,22 @@
 	 * @param {DOM node} tb - refrence to the DOM table object to be enhanced
 	 * @param {Object} options	- some customization values
 	 */
-	var init = function( tb, options){		
-		var t = $(tb);								//the table object is wrapped
-		if(options.disable) return destroy(t);		//the user is asking to destroy a previously colResized table
-		var	id = t.attr(ID) || SIGNATURE+count++;	//its id is obtained, if null new one is generated
-		if(!t.is("table") || tables[id]) return; 	//if the object is not a table or if it was already processed then it is ignored.
+	var init = function( tb, options){	
+		var t = $(tb);										//the table object is wrapped
+		if(options.disable) return destroy(t);				//the user is asking to destroy a previously colResized table
+		var	id = t.id = t.attr(ID) || SIGNATURE+count++;	//its id is obtained, if null new one is generated		
+		t.p = options.postbackSafe; 						//shortcut to detect postback safe 		
+		if(!t.is("table") || tables[id]) return; 			//if the object is not a table or if it was already processed then it is ignored.
 		t.addClass(SIGNATURE).attr(ID, id).before('<div class="JCLRgrips"/>');	//the grips container object is added. Signature class forces table rendering in fixed-layout mode to prevent column's min-width
 		t.opt = options; t.g = []; t.c = []; t.w = t.width(); t.gc = t.prev();	//t.c and t.g are arrays of columns and grips respectively				
+		if(options.marginLeft) t.gc.css("marginLeft", options.marginLeft);  	//if the table contains margins, it must be specified
+		if(options.marginRight) t.gc.css("marginRight", options.marginRight);  	//since there is no (direct) way to obtain margin values in its original units (%, em, ...)
 		t.cs = I(ie? tb.cellSpacing || tb.currentStyle.borderSpacing :t.css('border-spacing'))||2;	//table cellspacing (not even jQuery is fully cross-browser)
 		t.b  = I(ie? tb.border || tb.currentStyle.borderLeftWidth :t.css('border-left-width'))||1;	//outer border width (again cross-browser isues)
-		// if(!(tb.style.width || tb.width)) t.width(t.width()); //I am not an IE fan, but it is a pitty that only IE has the currentStyle attribute working as expected. For this reason I can not check easily if the table has an explicit width or if it is rendered as "auto"
-		tables[id] = t; 	//the table object is stored using its id as key		
+		// if(!(tb.style.width || tb.width)) t.width(t.width()); //I am not an IE fan at all, but it is a pitty that only IE has the currentStyle attribute working as expected. For this reason I can not check easily if the table has an explicit width or if it is rendered as "auto"
+		tables[id] = t; 	//the table object is stored using its id as key	
 		createGrips(t);		//grips are created
+	
 	};
 
 
@@ -75,27 +81,63 @@
 	 * @param {jQuery ref} t - table object
 	 */
 	var createGrips = function(t){	
-		var th = t.find("tr:first th");				//find all the column headers of the current table
-		if(!th.length) th = t.find("tr:first td"); 	//if none, the first TD row is used		
+	
+		var th = t.find(">thead>tr>th,>thead>tr>td");	//if table headers are specified in its semantically correct tag, are obtained
+		if(!th.length) th = t.find(">tbody>tr:first>th,>tr:first>th,>tbody>tr:first>td, >tr:first>td");	 //but headers can also be included in different ways
+		t.cg = t.find("col"); 						//a table can also contain a colgroup with col elements		
 		t.ln = th.length;							//table length is stored	
+		if(t.p && S && S[t.id])memento(t,th);		//if 'postbackSafe' is enabled and there is data for the current table, its coloumn layout is restored
 		th.each(function(i){						//iterate through the table column headers			
 			var c = $(this); 						//jquery wrap for the current column			
 			var g = $(t.gc.append('<div class="JCLRgrip"></div>')[0].lastChild); //add the visual node to be used as grip
 			g.t = t; g.i = i; g.c = c;	c.w =c.width();		//some values are stored in the grip's node data
 			t.g.push(g); t.c.push(c);						//the current grip and column are added to its table object
-			c.width(c.w).removeAttr('width');				//the width of the column is converted into pixel-based measurements
-			if (i < t.ln-1)	g.mousedown(onGripMouseDown).append(t.opt.gripInnerHtml).append('<div class="'+SIGNATURE+'"></div>'); //bind the mousedown event to start dragging 
+			c.width(c.w).removeAttr("width");				//the width of the column is converted into pixel-based measurements
+			if (i < t.ln-1)	g.mousedown(onGripMouseDown).append(t.opt.gripInnerHtml).append('<div class="'+SIGNATURE+'" style="cursor:'+t.opt.hoverCursor+'"></div>'); //bind the mousedown event to start dragging 
 			else g.addClass("JCLRLastGrip").removeClass("JCLRgrip");	//the last grip is used only to store data			
 			g.data(SIGNATURE, {i:i, t:t.attr(ID)});						//grip index and its table name are stored in the HTML 												
 		}); 	
-		syncGrips(t); //the grips are positioned according to the current table layout			
+		t.cg.removeAttr("width");	//remove the width attribute from elements in the colgroup (in any)
+		syncGrips(t); 				//the grips are positioned according to the current table layout			
 		//there is a small problem, some cells in the table could contain dimension values interfering with the 
 		//width value set by this plugin. Those values are removed
-		t.find('tr:not(:first)').find('td, th').each(function(){
-			$(this).removeAttr('width');		 
+		t.find('td, th').not(th).not('table th, table td').each(function(){  
+			$(this).removeAttr('width');	//the width attribute is removed from all table cells which are not nested in other tables and dont belong to the header
 		});		
+
+		
 	};
 	
+
+	/**
+	 * Function to allow the persistence of columns dimensions after a browser postback. It is based in
+	 * the HTML5 sessionStorage object, which can be emulated for older browsers using sessionstorage.js
+	 * @param {jQuery ref} t - table object
+	 * @param {jQuery ref} th - reference to the first row elements (only set in deserialization)
+	 */
+	var memento = function(t, th){ 
+		var w,m=0,i=0,aux =[];
+		if(th){										//in deserialization mode (after a postback)
+			t.cg.removeAttr("width");
+			if(t.opt.flush){ S[t.id] =""; return;} 	//if flush is activated, stored data is removed
+			w = S[t.id].split(";");					//column widths is obtained
+			for(;i<t.ln;i++){						//for each column
+				aux.push(100*w[i]/w[t.ln]+"%"); 	//width is stored in an array since it will be required again a couple of lines ahead
+				th.eq(i).css("width", aux[i] ); 	//each column width in % is resotred
+			}			
+			for(i=0;i<t.ln;i++)
+				t.cg.eq(i).css("width", aux[i]);	//this code is required in order to create an inline CSS rule with higher precedence than an existing CSS class in the "col" elements
+		}else{							//in serialization mode (after resizing a column)
+			S[t.id] ="";				//clean up previous data
+			for(i in t.c){				//iterate through columns
+				w = t.c[i].width();		//width is obtained
+				S[t.id] += w+";";		//width is appended to the sessionStorage object using ID as key
+				m+=w;					//carriage is updated to obtain the full size used by columns
+			}
+			S[t.id]+=m;					//the last item of the serialized string is the table's active area (width), 
+										//to be able to obtain % width value of each columns while deserializing
+		}	
+	};
 	
 	
 	/**
@@ -103,15 +145,16 @@
 	 * @param {jQuery ref} t - table object
 	 */
 	var syncGrips = function (t){	
-		t.gc.width(t.w);			//the grip's container width is updated 		
+		t.gc.width(t.w);			//the grip's container width is updated				
 		for(var i=0; i<t.ln; i++){	//for each column
 			var c = t.c[i]; 			
 			t.g[i].css({			//height and position of the grip is updated according to the table layout
 				left: c.offset().left - t.offset().left + c.outerWidth() + t.cs / 2 + PX,
-				height: t.opt.headerOnly? t.c[0].outerHeight() : t.outerHeight()
-			});												
+				height: t.opt.headerOnly? t.c[0].outerHeight() : t.outerHeight()				
+			});			
 		} 	
 	};
+	
 	
 	
 	/**
@@ -124,8 +167,9 @@
 	*/
 	var syncCols = function(t,i,isOver){
 		var inc = drag.x-drag.l, c = t.c[i], c2 = t.c[i+1]; 			
-		var w = c.w + inc;	var w2= c2.w- inc;	//their new width is obtained		
+		var w = c.w + inc;	var w2= c2.w- inc;	//their new width is obtained					
 		c.width( w + PX);	c2.width(w2 + PX);	//and set	
+		t.cg.eq(i).width( w + PX); t.cg.eq(i+1).width( w2 + PX);
 		if(isOver){c.w=w; c2.w=w2;}
 	};
 
@@ -139,12 +183,13 @@
 		var x = e.pageX - drag.ox + drag.l;		//next position according to horizontal mouse position increment
 		var mw = t.opt.minWidth, i = drag.i ;	//cell's min width
 		var l = t.cs*1.5 + mw + t.b;
-		
+
 		var max = i == t.ln-1? t.w-l: t.g[i+1].position().left-t.cs-mw; //max position according to the contiguous cells
 		var min = i? t.g[i-1].position().left+t.cs+mw: l;				//min position according to the contiguous cells
 		
 		x = M.max(min, M.min(max, x));						//apply boundings		
-		drag.x = x;	 drag.css("left",  x + PX); 			//apply position increment				
+		drag.x = x;	 drag.css("left",  x + PX); 			//apply position increment		
+			
 		if(t.opt.liveDrag){ 								//if liveDrag is enabled
 			syncCols(t,i); syncGrips(t);					//columns and grips are synchronized
 			var cb = t.opt.onDrag;							//check if there is an onDrag callback
@@ -169,6 +214,7 @@
 			syncCols(t,drag.i, true);	syncGrips(t);	//the columns and grips are updated
 			if (cb) { e.currentTarget = t[0]; cb(e); }	//if there is a callback function, it is fired
 		}	
+		if(t.p && S) memento(t); 						//if postbackSafe is enabled and there is sessionStorage support, the new layout is serialized and stored
 		drag = null;									//since the grip's dragging is over									
 	};	
 	
@@ -183,16 +229,12 @@
 		var t = tables[o.t],  g = t.g[o.i];			//shortcuts for the table and grip objects
 		g.ox = e.pageX;	g.l = g.position().left;	//the initial position is kept				
 		d.bind('mousemove.'+SIGNATURE, onGripDrag).bind('mouseup.'+SIGNATURE,onGripDragOver);	//mousemove and mouseup events are bound
-		h.append("<style type='text/css'>*{cursor:e-resize!important}</style>"); 				//change the mouse cursor to the horizontal-resizing pointer
+		h.append("<style type='text/css'>*{cursor:"+ t.opt.dragCursor +"!important}</style>"); 	//change the mouse cursor
 		g.addClass(t.opt.draggingClass); 	//add the dragging class (to allow some visual feedback)				
 		drag = g;							//the current grip is stored as the current dragging object
 		if(t.c[o.i].l) for(var i=0,c; i<t.ln; i++){ c=t.c[i]; c.l = false; c.w= c.width(); } 	//if the colum is locked (after browser resize), then c.w must be updated		
 		return false; 	//prevent text selection
 	};
-	
-	var log = function(t){
-		$("#log").html($("#log").html()+"; "+t);
-	}
 	
 	/**
 	 * Event handler fired when the browser is resized. The main purpose of this function is to update
@@ -235,6 +277,12 @@
 				liveDrag: false,				//enables table-layout updaing while dragging			
 				minWidth: 15, 					//minimum width value in pixels allowed for a column 
 				headerOnly: false,				//specifies that the size of the the column resizing anchors will be bounded to the size of the first row 
+				hoverCursor: "e-resize",  		//cursor to be used on grip hover
+				dragCursor: "e-resize",  		//cursor to be used while dragging
+				postbackSafe: false, 			//when it is enabled, table layout can persist after postback. It requires browsers with sessionStorage support (it can be emulated with sessionStorage.js). Some browsers ony 
+				flush: false, 					//when postbakSafe is enabled, and it is required to prevent layout restoration after postback, 'flush' will remove its associated layout data 
+				marginLeft: null,				//in case the table contains any margins, colResizable needs to know the values used, e.g. "10%", "15em", "5px" ...
+				marginRight: null, 				//in case the table contains any margins, colResizable needs to know the values used, e.g. "10%", "15em", "5px" ...
 				disable: false,					//disables all the enhancements performed in a previously colResized table	
 				
 				//events:
